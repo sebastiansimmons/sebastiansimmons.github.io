7201be5d7a5992a9adb8efa663310dc80ac43281
Sebastian Simmons <43860766+sebastiansimmons@users.noreply.github.com>
2024-02-02 17:50:04 -0800

    Replaced socials boilerplate

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Sebastian Simmons';
+export const SITE_DESCRIPTION = 'Sebastian has a website. This one!';
